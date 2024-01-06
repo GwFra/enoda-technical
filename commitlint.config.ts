c71cb75e7c8c91b94aa9759b6200523538a3078f
GwFra <george.franklin66@gmail.com>
2024-01-06 11:58:40 +0000

    chore(setup): Moved away from next eslint rules for default ones

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,6 +1,16 @@
 const config = {
     extends: [
         "@commitlint/config-conventional"
-    ]
+    ],
+    rules: {
+        "scope-enum": [
+            2,
+            "always",
+            [
+                "config",
+                "setup"
+            ]
+        ]
+    }
 }
 export default config;
