4383f18294f246f548466da4004a0c46e225eeb8
GwFra <george.franklin66@gmail.com>
2024-01-14 21:08:33 +0000

    refactor(setup): Auth is now checked throughout the page

--- app/api/auth/auth.config.ts
+++ app/api/auth/auth.config.ts
@@ -1,82 +1,23 @@
 import type { NextAuthConfig } from "next-auth";
-import Credentials from "next-auth/providers/credentials";
-import GoogleProvider from "next-auth/providers/google";
+import { NextRequest } from "next/server";
 
 export const authConfig: NextAuthConfig = {
   pages: {
     signIn: "/login",
   },
   callbacks: {
-    async jwt({ token, account, user }) {
-      console.log(user);
-      if (account) {
-        const res = await fetch(`http://localhost:1880/auth/login`, {
-          method: "POST",
-          headers: {
-            Authorization: `Bearer ${account?.id_token}`,
-          },
-        });
-        const resParsed = await res.json();
-        console.log(resParsed);
-      }
-      return token;
-    },
-    async authorized({ auth, request: { nextUrl } }) {
-      const isLoggedIn = !!auth?.user;
-      const isOnListings = nextUrl.pathname.startsWith("/listings");
-      if (isOnListings) {
-        if (isLoggedIn) return true;
+    async authorized({ request }: { request: NextRequest }) {
+      const isOnHome = request.nextUrl.pathname.startsWith("/home");
+      const token = request.cookies.get("access_token");
+      // validate token?
+      if (isOnHome) {
+        if (token) return true;
         return false;
-      } else if (isLoggedIn) {
-        return Response.redirect(new URL("listings", nextUrl));
+      } else if (token) {
+        return true;
       }
       return true;
     },
   },
-  providers: [
-    GoogleProvider({
-      clientId:
-        "807452420141-r11jc2hdpsrv2tqcrlfvjgsghm4uac8b.apps.googleusercontent.com",
-      clientSecret: "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",
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code",
-        },
-      },
-    }),
-    Credentials({
-      async authorize(credentials) {
-        const { email, password } = credentials;
-        let data = JSON.stringify({
-          email: "something@123.com",
-          password: "123",
-        });
-
-        let config = {
-          method: "post",
-          maxBodyLength: Infinity,
-          url: "localhost:1880/auth/login",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: "Basic YWRtaW46MTIz",
-          },
-          data: data,
-        };
-        const a = await fetch(data);
-        console.log(a);
-        console.log("WITHIN THE CREDENTIALS");
-        console.log(credentials);
-        return new Promise(() => {});
-        // return { id: "apple" };
-        // make request to backend
-      },
-    }),
-  ],
+  providers: [],
 };
-
-// # Copy the contents into a .env file and populate the values below if no default is provided
-// JWT_SECRET="ThisIsASecret"
-// GOOGLE_CLIENT_ID="807452420141-r11jc2hdpsrv2tqcrlfvjgsghm4uac8b.apps.googleusercontent.com"
-// GOOGLE_CLIENT_SECRET="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"
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,24 +1,7 @@
-// this is where the actions need to sit and wonder
-
-import { signIn } from "@/app/api/auth/auth";
-
-export async function authenticateEmailPaswsord(a: any, b: any) {
-  console.log("IN AUTHENTICATE WITH EMAIL PASSWORD");
-  console.log(a);
-  console.log(b);
+export async function authenticateWithGoogle(...options: any[]) {
+  console.log(options);
   try {
-    await signIn("credentials", b);
-    throw new Error("Ammple");
-  } catch (e) {
-    console.log(e);
-  }
-}
-
-export async function authenticateWithGoogle(a: any, b: any) {
-  console.log(a);
-  console.log(b);
-  try {
-    await signIn("google", b);
+    // await signIn("google", b);
   } catch (e) {
     console.log(e);
   }
