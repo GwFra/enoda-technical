7c303a910b16b371027f84e4b00490bcd1052df1
GwFra <george.franklin66@gmail.com>
2024-01-14 22:55:37 +0000

    feat(ui): Added pagination to display 3 previews at a time

--- app/ui/listings/container.tsx
+++ app/ui/listings/container.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { Box, List, ListItem } from "@mui/material";
+import React from "react";
+import { Box, Grid, List, ListItem, Pagination } from "@mui/material";
 import InfoPreview from "./preview";
 
 type Props = {
   toDisplay: any;
-  defaultListing?: boolean;
   userListing?: boolean;
   isBid?: boolean;
   isOffer?: boolean;
@@ -13,26 +13,59 @@ type Props = {
 
 export default function DisplayContainer(props: Props) {
   const { toDisplay, ...options } = props;
-  const listingsDisplay = toDisplay.map((display: any, index: number) => (
-    <ListItem
-      key={index}
-      style={{
-        margin: "20px 0px",
-      }}
-    >
-      <InfoPreview {...display} {...options} />
-    </ListItem>
-  ));
+
+  const maxDisplay = 3;
+  const [page, setPage] = React.useState(0);
+
+  const handlePageChange = (
+    _event: React.ChangeEvent<unknown>,
+    value: number
+  ) => {
+    setPage(value - 1);
+  };
+
+  const displaySplit = toDisplay.reduce(
+    (splits: any, _: any, index: number) => {
+      if (index % maxDisplay === 0)
+        splits.push(toDisplay.slice(index, index + maxDisplay));
+      return splits;
+    },
+    []
+  );
+
+  const listingsDisplay = displaySplit[page]?.map(
+    (display: any, index: number) => (
+      <ListItem
+        key={index}
+        style={{
+          margin: "20px 0px",
+        }}
+      >
+        <InfoPreview {...display} {...options} />
+      </ListItem>
+    )
+  );
   return (
     <Box
       style={{
-        display: "flex",
-        justifyContent: "center",
         borderRight: "2px solid black",
         height: "100%",
       }}
     >
-      <List style={{ width: "80%" }}>{listingsDisplay}</List>
+      <Grid container direction="column" alignItems="center">
+        <Grid item xs={9}>
+          <List style={{ width: "100%" }}>{listingsDisplay}</List>
+        </Grid>
+        <Grid item xs={2}>
+          <Pagination
+            defaultPage={1}
+            count={displaySplit.length}
+            page={page + 1}
+            onChange={handlePageChange}
+            size="large"
+          />
+        </Grid>
+      </Grid>
     </Box>
   );
 }
